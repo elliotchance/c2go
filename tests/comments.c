d4dcf08fd2977e6d6eb47efd2433e2bc0c5d62e9
Elliot Chance <elliotchance@gmail.com>
2017-02-27 01:24:39 +1100

    comments.c

---
+++ tests/comments.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main()
+{
+   // Single line comment in c source code
+ 
+   printf("Writing comments is very useful.\n");
+ 
+   /*
+    * Multi line comment syntax
+    * Comments help us to understand code later easily.
+    * Will you write comments while developing programs ?
+    */
+ 
+   printf("Good luck c programmer.\n"); 
+ 
+   return 0;
+}
