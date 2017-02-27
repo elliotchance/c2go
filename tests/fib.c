5567ac06ff422d54af4b3dde74e4ca3dc77621f5
Elliot Chance <elliotchance@gmail.com>
2017-02-27 19:15:22 +1100

    fib.c

---
+++ tests/fib.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main()
+{
+   int n = 5, first = 0, second = 1, next, c;
+ 
+   printf("First %d terms of Fibonacci series are :-\n",n);
+ 
+   for ( c = 0 ; c < n ; c++ )
+   {
+      if ( c <= 1 )
+         next = c;
+      else
+      {
+         next = first + second;
+         first = second;
+         second = next;
+      }
+      printf("%d\n",next);
+   }
+ 
+   return 0;
+}
