c986c2f003522ac1d9826eafae650a093adbc267
Elliot Chance <elliotchance@gmail.com>
2017-03-25 16:51:08 +1100

    Trying to fix regex

--- ast2json.py
+++ ast2json.py
@@ -57,7 +57,7 @@ regex = {
     'StringLiteral': r'^ (?P<address>[0-9a-fx]+) <(?P<position>.*)> \'(?P<type>.*)\'(?P<tags> lvalue)? (?P<value>.*)',
     'TranslationUnitDecl': r'^ (?P<address>[0-9a-fx]+)',
     'Typedef': r'^ (?P<address>[0-9a-fx]+) \'(?P<type>.*)\'',
-    'TypedefDecl': r"(?P<address>[0-9a-fx]+) <(?P<position>.+?)> (?P<position2><invalid sloc>|0x[0-9a-f]+)?(?P<tags>.*?)?(?P<name>\w+) '(?P<type>.*?)'(?P<type2>:'.*?')?",
+    'TypedefDecl': r"(?P<address>[0-9a-fx]+) <(?P<position>.+?)> (?P<position2><invalid sloc>|0x[0-9a-f]+)?(?P<tags>.*?)(?P<name>\w+) '(?P<type>.*?)'(?P<type2>:'.*?')?",
     'TypedefType': r'^ (?P<address>[0-9a-fx]+) \'(?P<type>.*)\' (?P<tags>.+)',
     'UnaryOperator': r"^ (?P<address>[0-9a-fx]+) <(?P<position>.*)> '(?P<type>.*?)'(?P<tags1> lvalue)?(?P<tags2> prefix)?(?P<tags3> postfix)? '(?P<operator>.*?)'",
     'VarDecl': r"^ (?P<address>[0-9a-fx]+) <(?P<position>.*)> (?P<position2>[^ ]+) (?P<name>.+) '(?P<type>.+?)'.*?(?P<tags>.*)",
