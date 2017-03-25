d68178fae225719135764c1d4ea7ca06f4aef570
Elliot Chance <elliotchance@gmail.com>
2017-03-25 16:38:09 +1100

    Check ast2json in each test

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,13 @@
 for TEST in $(ls -1 tests); do
     echo $TEST
 
+    # First check that ast2json.py can understand every line of the clang AST.
+    clang -Xclang -ast-dump -fsyntax-only tests/argv.c | python ast2json.py > /tmp/0.txt
+    if [ $? != 0 ]; then
+        cat /tmp/0.txt
+        exit 1
+    fi
+
     clang tests/$TEST
     (echo "7" | ./a.out some args) > /tmp/1.txt
 
