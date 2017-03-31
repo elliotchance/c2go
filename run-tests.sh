41e228cfd71283ddbef6f87249b9972eefd08a46
Elliot Chance <elliotchance@gmail.com>
2017-04-01 10:22:13 +1100

    Clean up

--- run-tests.sh
+++ run-tests.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+CLANG_BIN=${CLANG_BIN:-clang}
+CLANG_VERSION=$(CLANG_BIN --version)
+
+echo -e "CLANG_BIN=$CLANG_BIN\n"
+echo -e "CLANG_VERSION=$CLANG_VERSION\n"
+
 function run_test {
     export TEST=$1
 
     echo $TEST
 
     # First check that ast2json.py can understand every line of the clang AST.
-    clang -Xclang -ast-dump -fsyntax-only $TEST | python ast2json.py > /tmp/0.txt
+    $CLANG_BIN -Xclang -ast-dump -fsyntax-only $TEST | python ast2json.py > /tmp/0.txt
     if [ $? != 0 ]; then
         cat /tmp/0.txt
         exit 1
     fi
 
     # Compile with clang
-    clang -lm $TEST
+    $CLANG_BIN -lm $TEST
     if [ $? != 0 ]; then
         exit 1
     fi
