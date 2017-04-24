c84e8ce556e6b2bb9730587512d13d07a5650c2e
Elliot Chance <elliotchance@gmail.com>
2017-04-24 19:50:47 +1000

    Reduce stdio.h tests into one file

--- tests/stdio/puts.c
+++
@@ -1,8 +0,0 @@
-// This program actually still works without including stdio.h but it should be
-// here for consistency.
-#include <stdio.h>
-
-int main() {
-	puts("c2go");
-	return 0;
-}
---
+++ tests/stdio/stdio.c
@@ -0,0 +1,23 @@
+// This program actually still works without including stdio.h but it should be
+// here for consistency.
+#include <stdio.h>
+
+void test_putchar()
+{
+    char c;
+    for (c = 'A'; c <= 'Z'; c++)
+	putchar(c);
+}
+
+void test_puts()
+{
+    puts("c2go");
+}
+
+int main()
+{
+	test_putchar();
+	test_puts();
+
+    return 0;
+}
