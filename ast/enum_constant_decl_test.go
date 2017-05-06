c534686de375816645a79bf583cbf578ee9958f0
x-tyger <x-tyger@users.noreply.github.com>
2017-05-06 18:37:41 +0000

    Handle 'referenced' in EnumConstantDecl

--- ast/enum_constant_decl_test.go
+++ ast/enum_constant_decl_test.go
@@ -7,12 +7,22 @@ import (
 func TestEnumConstantDecl(t *testing.T) {
 	nodes := map[string]Node{
 		`0x1660db0 <line:185:3> __codecvt_noconv 'int'`: &EnumConstantDecl{
-			Address:   "0x1660db0",
-			Position:  "line:185:3",
-			Position2: "",
-			Name:      "__codecvt_noconv",
-			Type:      "int",
-			Children:  []Node{},
+			Address:    "0x1660db0",
+			Position:   "line:185:3",
+			Position2:  "",
+			Referenced: false,
+			Name:       "__codecvt_noconv",
+			Type:       "int",
+			Children:   []Node{},
+		},
+		`0x3c77ba8 <line:59:3, col:65> col:3 referenced _ISalnum 'int'`: &EnumConstantDecl{
+			Address:    "0x3c77ba8",
+			Position:   "line:59:3, col:65",
+			Position2:  "col:3",
+			Referenced: true,
+			Name:       "_ISalnum",
+			Type:       "int",
+			Children:   []Node{},
 		},
 	}
 
