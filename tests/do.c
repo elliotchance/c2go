02c5b1adb00aaa497453deec6999dfdf415d702f
Elliot Chance <elliotchance@gmail.com>
2017-05-31 00:02:09 +0200

    do.c

--- tests/do.c
+++ tests/do.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include "tests.h"
 
-int main() {
+int main()
+{
+    plan(5);
+	
 	int i = 0;
 
+	// There will be 4 checks in the first loop.
 	do {
-		printf("do loop %d\n", i);
+		pass("%s", "first do statement");
 		i = i + 1;
 	} while( i < 4 );
 
-	// continue
+	// Only one check in the second loop.
 	i = 0;
 	do {
 		i++;
 		if(i < 3) continue;
-		printf("%d\n", i);
+		pass("%s", "second do statement");
 	} while(i < 3);
 
+	done_testing();
 }
