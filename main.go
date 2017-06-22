2fc5662eeaf5079757f30d529fbf74c37f05418e
Elliot Chance <elliotchance@gmail.com>
2017-06-23 08:04:03 +1000

    Bump version

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ import (
 //     c2go -v
 //
 // See https://github.com/elliotchance/c2go/wiki/Release-Process
-const Version = "0.13.3"
+const Version = "0.13.4"
 
 // ProgramArgs - arguments of program
 type ProgramArgs struct {
