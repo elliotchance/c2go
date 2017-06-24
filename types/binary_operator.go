62519a554dea9c6d8992e42eb66f72d6cde0fa87
konstantin8105 <konstantin8105@gmail.com>
2017-06-24 22:54:59 +0300

    add comment. Fix #154

--- types/binary_operator.go
+++ types/binary_operator.go
@@ -14,5 +14,17 @@ func ResolveTypeForBinaryOperator(p *program.Program, operator, leftType, rightT
 		return "bool"
 	}
 
+	/*
+		Example of c code:
+		int fact(int n) {
+			int lcv, p;
+			for(p=1, lcv=2; lcv <= n; p=p*lcv, lcv++); // <<== Here 2 timer in initialization and increment
+			return p;
+		}
+	*/
+	if operator == "," {
+		panic("Now, Algorithm is not fully ready")
+	}
+
 	return leftType
 }
