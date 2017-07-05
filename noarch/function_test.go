b25723794078e2d150d9a2aaeef98ea1afd00042
Konstantin8105 <konstantin8105@gmail.com>
2017-07-05 15:01:25 +0300

    add test for noarch

---
+++ noarch/function_test.go
@@ -0,0 +1,41 @@
+package noarch
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBoolToInt(t *testing.T) {
+	tests := []struct {
+		input bool
+		want  int
+	}{
+		{true, 1},
+		{false, 0},
+	}
+	for _, tt := range tests {
+		name := fmt.Sprintf("%#v", tt.input)
+
+		t.Run(name, func(t *testing.T) {
+			if got := BoolToInt(tt.input); got != tt.want {
+				t.Errorf("BoolToInt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func test_b() interface{} {
+	return "b"
+}
+func test_c() interface{} {
+	return "c"
+}
+
+func TestTernary(t *testing.T) {
+	if result := Ternary(true, test_b, test_c); result.(string) != "b" {
+		t.Errorf("Ternary - true is Fail")
+	}
+	if result := Ternary(false, test_b, test_c); result.(string) != "c" {
+		t.Errorf("Ternary - false is Fail")
+	}
+}
