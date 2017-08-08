7a7f13d11328c6d8b76a2dcf6fbdc3e26d2d8513
Elliot Chance <elliotchance@gmail.com>
2017-08-09 09:33:43 +1000

    Bump version

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ import (
 //     c2go -v
 //
 // See https://github.com/elliotchance/c2go/wiki/Release-Process
-const Version = "0.14.0"
+const Version = "0.14.1"
 
 // ProgramArgs defines the options available when processing the program. There
 // is no constructor since the zeroed out values are the appropriaye defaults -
