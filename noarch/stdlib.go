ac13eccde12bd395ff04d101a7c3f8d464d813b5
Elliot Chance <elliotchance@gmail.com>
2017-08-19 15:00:17 +1000

    atof()

--- noarch/stdlib.go
+++ noarch/stdlib.go
@@ -1,7 +1,10 @@
 package noarch
 
 import (
+	"math"
+	"regexp"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -16,6 +19,116 @@ func Abs(n int) int {
 	}
 
 	return n
+}
+
+// Atof parses the C string str, interpreting its content as a floating point
+// number and returns its value as a double.
+//
+// The function first discards as many whitespace characters (as in isspace) as
+// necessary until the first non-whitespace character is found. Then, starting
+// from this character, takes as many characters as possible that are valid
+// following a syntax resembling that of floating point literals (see below),
+// and interprets them as a numerical value. The rest of the string after the
+// last valid character is ignored and has no effect on the behavior of this
+// function.
+//
+// C90 (C++98): A valid floating point number for atof using the "C" locale is
+// formed by an optional sign character (+ or -), followed by a sequence of
+// digits, optionally containing a decimal-point character (.), optionally
+// followed by an exponent part (an e or E character followed by an optional
+// sign and a sequence of digits).
+//
+// C99/C11 (C++11): A valid floating point number for atof using the "C" locale
+// is formed by an optional sign character (+ or -), followed by one of:
+//
+//   - A sequence of digits, optionally containing a decimal-point character
+//     (.), optionally followed by an exponent part (an e or E character
+//     followed by an optional sign and a sequence of digits).
+//   - A 0x or 0X prefix, then a sequence of hexadecimal digits (as in isxdigit)
+//     optionally containing a period which separates the whole and fractional
+//     number parts. Optionally followed by a power of 2 exponent (a p or P
+//     character followed by an optional sign and a sequence of hexadecimal
+//     digits).
+//   - INF or INFINITY (ignoring case).
+//   - NAN or NANsequence (ignoring case), where sequence is a sequence of
+//     characters, where each character is either an alphanumeric character (as
+//     in isalnum) or the underscore character (_).
+//
+// If the first sequence of non-whitespace characters in str does not form a
+// valid floating-point number as just defined, or if no such sequence exists
+// because either str is empty or contains only whitespace characters, no
+// conversion is performed and the function returns 0.0.
+func Atof(str []byte) float64 {
+	// First start by removing any trailing whitespace.
+	s := strings.TrimSpace(CStringToString(str))
+
+	// Before we get into the more complicated parser below lets just try and
+	// interpret the number.
+	f, err := strconv.ParseFloat(s, 64)
+	if err == nil {
+		return f
+	}
+
+	// Now convert to lowercase, this makes the regexp and comparisons easier
+	// and doesn't change the value.
+	s = strings.ToLower(s)
+
+	// 1. Hexadecimal integer? This must be checked before floating-point
+	// because it starts with a 0.
+	r := regexp.MustCompile(`^([+-])?0x([0-9a-f]+)(p[-+]?[0-9a-f]+)?`)
+	match := r.FindStringSubmatch(s)
+	if match != nil {
+		n, err := strconv.ParseUint(match[2], 16, 32)
+		if err == nil {
+			f := float64(n)
+
+			if match[1] == "-" {
+				f *= -1
+			}
+
+			if match[3] != "" {
+				p, err := strconv.Atoi(match[3][1:])
+				if err != nil {
+					return 0
+				}
+
+				f *= math.Pow(2, float64(p))
+			}
+
+			return f
+		}
+
+		return 0
+	}
+
+	// 2. Floating-point number?
+	r = regexp.MustCompile(`^[+-]?\d*(\.\d*)?(e[+-]\d+)?`)
+	match = r.FindStringSubmatch(s)
+	if match != nil {
+		f, err := strconv.ParseFloat(match[0], 64)
+		if err == nil {
+			return f
+		}
+	}
+
+	// 3. Infinity?
+	if s == "infinity" || s == "+infinity" ||
+		s == "inf" || s == "+inf" {
+		return math.Inf(1)
+	}
+	if s == "-infinity" || s == "-inf" {
+		return math.Inf(-1)
+	}
+
+	// 4. Not a number?
+	if len(s) > 2 && s[:3] == "nan" {
+		return math.NaN()
+	}
+	if len(s) > 3 && s[1:4] == "nan" {
+		return math.NaN()
+	}
+
+	return 0
 }
 
 // Atoi parses the C-string str interpreting its content as an integral number,
