8dc4d7f378d5206e1504d37542ce0dcd69ea3384
Elliot Chance <elliotchance@gmail.com>
2017-09-10 00:36:28 +1000

    Bump version

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ import (
 //     c2go -v
 //
 // See https://github.com/elliotchance/c2go/wiki/Release-Process
-const Version = "v0.16.0 Radium 2017-09-04"
+const Version = "v0.16.1 Radium 2017-09-09"
 
 var stderr io.Writer = os.Stderr
 
