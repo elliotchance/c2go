6a4f84363decb3538fd1a19404e71dee6f29d6c1
Konstantin8105 <Konstantin8105@gmail.com>
2017-10-07 17:46:16 +0300

    simplification

--- main.go
+++ main.go
@@ -14,10 +14,6 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	goast "go/ast"
-	"go/format"
-	"go/parser"
-	"go/token"
 	"io"
 	"io/ioutil"
 	"os"
@@ -25,19 +21,15 @@ import (
 	"path"
 	"path/filepath"
 	"regexp"
-	"sort"
 	"strings"
 
 	"errors"
 	"reflect"
 
+	cleaner "github.com/Konstantin8105/Cleaner"
 	"github.com/elliotchance/c2go/ast"
 	"github.com/elliotchance/c2go/program"
 	"github.com/elliotchance/c2go/transpiler"
-	"honnef.co/go/tools/lint/lintutil"
-	"honnef.co/go/tools/unused"
-
-	"golang.org/x/tools/imports"
 )
 
 // Version can be requested through the command line with:
@@ -255,233 +247,14 @@ func Start(args ProgramArgs) error {
 
 	err = ioutil.WriteFile(outputFilePath, []byte(p.String()), 0755)
 	if err != nil {
-		return fmt.Errorf("writing C output file failed: %v", err)
+		return fmt.Errorf("writing Go output file failed: %v", err)
 	}
 
 	if !*keepUnused {
-		err := removeUnused(outputFilePath)
+		err := cleaner.Go(outputFilePath, outputFilePath)
 		if err != nil {
 			return fmt.Errorf("Cannot remove unused: %v", err)
 		}
-	}
-
-	return nil
-}
-
-func removeUnused(filename string) error {
-
-	// prepare linter
-	var mode unused.CheckMode
-	mode |= unused.CheckConstants
-	//mode |= unused.CheckFields
-	mode |= unused.CheckFunctions
-	mode |= unused.CheckTypes
-	mode |= unused.CheckVariables
-	checker := unused.NewChecker(mode)
-	l := unused.NewLintChecker(checker)
-
-	fs := lintutil.FlagSet("unused")
-	err := fs.Parse([]string{filename})
-	if err != nil {
-		return fmt.Errorf("Error in flag parsing : %v", err)
-	}
-
-	// take result of linter work
-	ps, _, err := lintutil.Lint(l, fs.Args(), &lintutil.Options{})
-	if err != nil {
-		return fmt.Errorf("Error in linter : %v", err)
-	}
-
-	// linter is not found any unused elements
-	if len(ps) == 0 {
-		return nil
-	}
-
-	// prepare structures for analyzing
-	type un int
-
-	const (
-		unusedConstantans un = iota
-		unusedFunction
-		unusedType
-		unusedVariable
-	)
-	const (
-		suffix = " is unused (U1000)"
-	)
-
-	var unusedMap = map[string]un{
-		"const": unusedConstantans,
-		"func":  unusedFunction,
-		"type":  unusedType,
-		"var":   unusedVariable,
-	}
-
-	// create ast tree
-	fset := token.NewFileSet()
-	tree, err := parser.ParseFile(fset, filename, nil, 0)
-	if err != nil {
-		return fmt.Errorf("Error: Cannot parse : %v", err)
-	}
-
-	// parse unused strings
-	type unusedParameter struct {
-		u        un
-		name     string
-		position token.Pos
-	}
-	var unusedParameters []unusedParameter
-	for _, p := range ps {
-		p.Text = p.Text[0 : len(p.Text)-len(suffix)]
-		for k, v := range unusedMap {
-			if strings.Contains(p.Text, k) {
-				unusedParameters = append(unusedParameters, unusedParameter{
-					u:        v,
-					name:     strings.TrimSpace(p.Text[len(k):len(p.Text)]),
-					position: p.Position,
-				})
-			}
-		}
-	}
-
-	var removeItems []int
-
-	// remove unused parts of AST tree
-	for _, param := range unusedParameters {
-		switch param.u {
-
-		// remove unused constants
-		case unusedConstantans:
-			{
-				for i := 0; i < len(tree.Decls); i++ {
-					gen, ok := tree.Decls[i].(*goast.GenDecl)
-					if !ok || gen == (*goast.GenDecl)(nil) || gen.Tok != token.CONST {
-						goto nextConstDecl
-					}
-					if s, ok := gen.Specs[0].(*goast.ValueSpec); ok {
-						for _, n := range s.Names {
-							//fmt.Println("C{", param.position, ",", s.Pos(), "}")
-							if strings.Contains(n.String(), param.name) && param.position == s.Pos() {
-								//fmt.Println("*")
-								removeItems = append(removeItems, i)
-								continue
-							}
-						}
-					}
-				nextConstDecl:
-				}
-			}
-
-		// remove unused functions
-		case unusedFunction:
-			{
-				for i := 0; i < len(tree.Decls); i++ {
-					gen, ok := tree.Decls[i].(*goast.FuncDecl)
-					if !ok || gen == (*goast.FuncDecl)(nil) {
-						goto nextFuncDecl
-					}
-					//fmt.Println("F{", param.position, ",", gen.Pos(), ",", gen.End(), "}")
-					if strings.Contains(gen.Name.String(), param.name) && gen.Pos() <= param.position && param.position <= gen.End() {
-						//fmt.Println("*")
-						removeItems = append(removeItems, i)
-						continue
-					}
-
-				nextFuncDecl:
-				}
-			}
-
-		// remove unused types
-		case unusedType:
-			{
-				for i := 0; i < len(tree.Decls); i++ {
-					gen, ok := tree.Decls[i].(*goast.GenDecl)
-					if !ok || gen == (*goast.GenDecl)(nil) || gen.Tok != token.TYPE {
-						goto nextTypeDecl
-					}
-					if s, ok := gen.Specs[0].(*goast.TypeSpec); ok {
-						//fmt.Println("T{", param.position, ",", s.Pos(), "}")
-						if strings.Contains(s.Name.String(), param.name) && param.position == s.Pos() {
-							//fmt.Println("*")
-							removeItems = append(removeItems, i)
-							continue
-						}
-					}
-				nextTypeDecl:
-				}
-			}
-
-		// remove unused variables
-		case unusedVariable:
-			{
-				for i := 0; i < len(tree.Decls); i++ {
-					gen, ok := tree.Decls[i].(*goast.GenDecl)
-					if !ok || gen == (*goast.GenDecl)(nil) || gen.Tok != token.VAR {
-						goto nextVarDecl
-					}
-					if s, ok := gen.Specs[0].(*goast.ValueSpec); ok {
-						for _, n := range s.Names {
-							//fmt.Println("V{", param.position, ",", s.Pos(), "}")
-							if strings.Contains(n.String(), param.name) && param.position == s.Pos() {
-								//fmt.Println("*")
-								removeItems = append(removeItems, i)
-								continue
-							}
-						}
-					}
-				nextVarDecl:
-				}
-			}
-
-		}
-	}
-	//TODO removing methods of types
-	// example of type:
-	// 		type pthread_attr_t [56]byte
-	// example of unused result:
-	// 		exit.go:135:6: type pthread_attr_t is unused (U1000)
-	// example of that type method:
-	// 		func (self *pthread_attr_t) cast(t reflect.Type) reflect.Value {
-
-	sort.Ints(removeItems)
-
-	// TODO optimize
-	// remove Decl element from tree
-	for j := len(removeItems) - 1; j >= 0; j-- {
-		i := removeItems[j]
-		var rr []goast.Decl
-		if i != 0 {
-			rr = append(rr, tree.Decls[0:i]...)
-		}
-		if i != len(tree.Decls)-1 {
-			rr = append(rr, tree.Decls[i+1:len(tree.Decls)]...)
-		}
-		tree.Decls = rr
-	}
-
-	// convert AST tree to Go code
-	var buf bytes.Buffer
-	err = format.Node(&buf, fset, tree)
-	if err != nil {
-		return fmt.Errorf("Error: convert AST tree to Go code : %v", err)
-	}
-
-	// Remove imports
-	b, err := imports.Process(filename, buf.Bytes(), nil)
-	if err != nil {
-		return fmt.Errorf("Error: Cannot modify imports : %v", err)
-	}
-	buf = *bytes.NewBuffer(b)
-
-	// write buffer with Go code to file
-	err = ioutil.WriteFile(filename, buf.Bytes(), 0755)
-	if err != nil {
-		return fmt.Errorf("writing C output file failed: %v", err)
-	}
-
-	// recursive checking
-	if len(ps) != 0 {
-		return removeUnused(filename)
 	}
 
 	return nil
@@ -504,10 +277,10 @@ var (
 	verboseFlag       = transpileCommand.Bool("V", false, "print progress as comments")
 	outputFlag        = transpileCommand.String("o", "", "output Go generated code to the specified file")
 	packageFlag       = transpileCommand.String("p", "main", "set the name of the generated package")
+	keepUnused        = transpileCommand.Bool("keep-unused", false, "Keep unused constants, functions, ...")
 	transpileHelpFlag = transpileCommand.Bool("h", false, "print help information")
 	astCommand        = flag.NewFlagSet("ast", flag.ContinueOnError)
 	astHelpFlag       = astCommand.Bool("h", false, "print help information")
-	keepUnused        = transpileCommand.Bool("keep-unused", false, "Keep unused constants, functions, ...")
 )
 
 func main() {
