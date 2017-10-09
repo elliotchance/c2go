8b979288d5e60400cb0422126edf0e31fd6e9da9
Konstantin8105 <Konstantin8105@gmail.com>
2017-10-09 16:35:06 +0300

    travis timeout fight

--- travis/test.sh
+++ travis/test.sh
@@ -40,16 +40,10 @@ GOLIST=/tmp/golist.txt
 go list -f 'go test -v -tags integration -race -covermode atomic -coverprofile {{.Name}}.coverprofile -coverpkg $PKGS_DELIM {{.ImportPath}}' $PKGS > $GOLIST
 echo "Show go list fully:"
 cat $GOLIST
-#declare -a GoList=(`cat "$GOLIST"`)
-#for i in "${GoList[@]}"
-#do
-#	echo "Next : $i"
-#	xargs -I{} bash -c "{} >> $OUTFILE" < "$i"
-#done
 while IFS= read -r line
 do 
-	echo "$line\n"
-	xargs -I{} bash -c "{} >> $OUTFILE" < "$line" 
+	echo "Starting : $line"
+	xargs -I{} bash -c "{} >> $OUTFILE" "$line" 
 done < "$GOLIST"
 
 
