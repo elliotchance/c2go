57ce14dccaf9d15a2c8e083b3f0f4a5a012e9f0f
Konstantin8105 <konstantin8105@gmail.com>
2017-10-18 17:02:26 +0300

    cleaning from debug information

--- transpiler/enum.go
+++ transpiler/enum.go
@@ -3,8 +3,6 @@
 package transpiler
 
 import (
-	"fmt"
-	"go/parser"
 	"go/token"
 
 	goast "go/ast"
@@ -105,97 +103,14 @@ func transpileEnumConstantDecl(p *program.Program, n *ast.EnumConstantDecl) (
 	}, preStmts, postStmts
 }
 
-/*
-Example of AST tree:
-|-EnumDecl 0x32fb5a0 <enum.c:3:1, col:45> col:6 week
-| |-EnumConstantDecl 0x32fb650 <col:11> col:11 Mon 'int'
-| |-EnumConstantDecl 0x32fb6a0 <col:16> col:16 Tue 'int'
-*/
-/*
-type w int
-
-const (
- A w = iota
- B
-)
-   23  .  .  1: *ast.GenDecl {
-   24  .  .  .  TokPos: 7:1
-   25  .  .  .  Tok: type
-   26  .  .  .  Lparen: -
-   27  .  .  .  Specs: []ast.Spec (len = 1) {
-   28  .  .  .  .  0: *ast.TypeSpec {
-   29  .  .  .  .  .  Name: *ast.Ident {
-   30  .  .  .  .  .  .  NamePos: 7:6
-   31  .  .  .  .  .  .  Name: "w"
-   32  .  .  .  .  .  .  Obj: *ast.Object {
-   33  .  .  .  .  .  .  .  Kind: type
-   34  .  .  .  .  .  .  .  Name: "w"
-   35  .  .  .  .  .  .  .  Decl: *(obj @ 28)
-   36  .  .  .  .  .  .  }
-   37  .  .  .  .  .  }
-   38  .  .  .  .  .  Type: *ast.Ident {
-   39  .  .  .  .  .  .  NamePos: 7:8
-   40  .  .  .  .  .  .  Name: "int"
-   41  .  .  .  .  .  }
-   42  .  .  .  .  }
-   43  .  .  .  }
-   44  .  .  .  Rparen: -
-   45  .  .  }
-
-   46  .  .  2: *ast.GenDecl {
-   47  .  .  .  TokPos: 9:1
-   48  .  .  .  Tok: const
-   49  .  .  .  Lparen: 9:7
-   50  .  .  .  Specs: []ast.Spec (len = 2) {
-   51  .  .  .  .  0: *ast.ValueSpec {
-   52  .  .  .  .  .  Names: []*ast.Ident (len = 1) {
-   53  .  .  .  .  .  .  0: *ast.Ident {
-   54  .  .  .  .  .  .  .  NamePos: 10:2
-   55  .  .  .  .  .  .  .  Name: "A"
-   56  .  .  .  .  .  .  .  Obj: *ast.Object {
-   57  .  .  .  .  .  .  .  .  Kind: const
-   58  .  .  .  .  .  .  .  .  Name: "A"
-   59  .  .  .  .  .  .  .  .  Decl: *(obj @ 51)
-   60  .  .  .  .  .  .  .  .  Data: 0
-   61  .  .  .  .  .  .  .  }
-   62  .  .  .  .  .  .  }
-   63  .  .  .  .  .  }
-   64  .  .  .  .  .  Type: *ast.Ident {
-   65  .  .  .  .  .  .  NamePos: 10:4
-   66  .  .  .  .  .  .  Name: "w"
-   67  .  .  .  .  .  .  Obj: *(obj @ 32)
-   68  .  .  .  .  .  }
-   69  .  .  .  .  .  Values: []ast.Expr (len = 1) {
-   70  .  .  .  .  .  .  0: *ast.Ident {
-   71  .  .  .  .  .  .  .  NamePos: 10:8
-   72  .  .  .  .  .  .  .  Name: "iota"
-   73  .  .  .  .  .  .  }
-   74  .  .  .  .  .  }
-   75  .  .  .  .  }
-   76  .  .  .  .  1: *ast.ValueSpec {
-   77  .  .  .  .  .  Names: []*ast.Ident (len = 1) {
-   78  .  .  .  .  .  .  0: *ast.Ident {
-   79  .  .  .  .  .  .  .  NamePos: 11:2
-   80  .  .  .  .  .  .  .  Name: "B"
-   81  .  .  .  .  .  .  .  Obj: *ast.Object {
-   82  .  .  .  .  .  .  .  .  Kind: const
-   83  .  .  .  .  .  .  .  .  Name: "B"
-   84  .  .  .  .  .  .  .  .  Decl: *(obj @ 76)
-   85  .  .  .  .  .  .  .  .  Data: 1
-   86  .  .  .  .  .  .  .  }
-   87  .  .  .  .  .  .  }
-   88  .  .  .  .  .  }
-   89  .  .  .  .  }
-   90  .  .  .  }
-   91  .  .  .  Rparen: 12:1
-   92  .  .  }
-*/
 func transpileEnumDecl(p *program.Program, n *ast.EnumDecl) error {
 	preStmts := []goast.Stmt{}
 	postStmts := []goast.Stmt{}
 
 	theType, err := types.ResolveType(p, "int")
-	//p.AddMessage(p.GenerateWarningMessage(err, n))
+	if err != nil {
+		p.AddMessage(p.GenerateWarningMessage(err, n))
+	}
 	baseType := util.NewTypeIdent(theType)
 
 	bt := goast.GenDecl{
@@ -210,11 +125,7 @@ func transpileEnumDecl(p *program.Program, n *ast.EnumDecl) error {
 			},
 		},
 	}
-	/*
-		goast.Fprint(os.Stdout, token.NewFileSet(), bt, func(name string, value reflect.Value) bool {
-			return true
-		})
-	*/
+
 	p.File.Decls = append(p.File.Decls, &bt)
 
 	baseType2 := util.NewTypeIdent(n.Name)
@@ -253,119 +164,15 @@ func transpileEnumDecl(p *program.Program, n *ast.EnumDecl) error {
 		e.Names[0].Obj.Data = i
 		decl.Specs = append(decl.Specs, e)
 	}
-	/*
-	   54  .  .  1: *ast.ValueSpec {
-	   55  .  .  .  Doc: nil
-	   56  .  .  .  Names: []*ast.Ident (len = 1) {
-	   57  .  .  .  .  0: *ast.Ident {
-	   58  .  .  .  .  .  NamePos: -
-	   59  .  .  .  .  .  Name: "wwwb"
-	   60  .  .  .  .  .  Obj: *ast.Object {
-	   61  .  .  .  .  .  .  Kind: const
-	   62  .  .  .  .  .  .  Name: "wwwb"
-	   63  .  .  .  .  .  .  Decl: nil
-	   64  .  .  .  .  .  .  Data: 1
-	   65  .  .  .  .  .  .  Type: nil
-	   66  .  .  .  .  .  }
-	   67  .  .  .  .  }
-	   68  .  .  .  }
-	   69  .  .  .  Type: nil
-	   70  .  .  .  Values: nil
-	   71  .  .  .  Comment: nil
-	   72  .  .  }
-	*/
-	decl.Specs = append(decl.Specs, &goast.ValueSpec{
-		Names: []*goast.Ident{
-			&goast.Ident{
-				Name: "asdasdasd",
-				Obj: &goast.Object{
-					Kind: goast.Con,
-					Name: "asdasdasd",
-				},
-			},
-		},
-	})
-	// fmt.Println("ABDSD")
-
-	/*
-		goast.Fprint(os.Stdout, token.NewFileSet(), decl, func(name string, value reflect.Value) bool {
-			return true
-		})
-	*/
-	fmt.Println("\n\n\n\n=================")
-
-	src := `package main
-
-type WWW2 int
-
-const (
-	wwwa2 WWW2 = iota
-	wwwb2
-	wwwc2
-)
-`
-
-	// Create the AST by parsing src.
-	fset2 := token.NewFileSet() // positions are relative to fset
-	f3, err := parser.ParseFile(fset2, "", src, 0)
-	if err != nil {
-		panic(err)
-	}
-
-	f3.Decls = []goast.Decl{f3.Decls[1]}
-	f3.Scope = nil
-	f3.Unresolved = nil
-	f3.Name = nil
-	fset2 = token.NewFileSet()
-
-	decl2 := f3.Decls[0].(*goast.GenDecl)
-
-	decl2.Specs[0].(*goast.ValueSpec).Names[0].Obj.Decl = nil
-	decl2.Specs[0].(*goast.ValueSpec).Type.(*goast.Ident).Obj = nil
-	decl2.Specs[1].(*goast.ValueSpec).Names[0].Obj = nil
-	decl2.Specs[2].(*goast.ValueSpec).Names[0].Obj = nil
 
 	decl.Specs[0].(*goast.ValueSpec).Names[0].Obj.Decl = nil
 	decl.Specs[0].(*goast.ValueSpec).Type.(*goast.Ident).Obj = nil
 	decl.Specs[1].(*goast.ValueSpec).Names[0].Obj = nil
 	decl.Specs[2].(*goast.ValueSpec).Names[0].Obj = nil
 
-	//decl.Specs = append(decl.Specs, decl2.Specs...)
-	//decl2.Specs = append(decl2.Specs, decl.Specs...)
-
-	//decl2.Specs[2].(*goast.ValueSpec).Names[0] = nil
-	//decl2.Rparen = 0
-	//decl2.Lparen = 0
 	decl.Lparen = 1
 
-	p.File.Decls = append(p.File.Decls, []goast.Decl{decl}...)
-	p.File.Decls = append(p.File.Decls, decl2)
 	p.File.Decls = append(p.File.Decls, decl)
-	p.File.Decls = append(p.File.Decls, decl2)
-	p.File.Decls = append(p.File.Decls, decl2)
-
-	// Print the AST.
-	/*
-		fmt.Printf("fset2 = %#v\n", fset2)
-
-		fmt.Printf("decl  =%#v\n", decl)
-		fmt.Printf("decl2 = %#v\n", decl2)
-
-		fmt.Printf("f3-decl = %#v\n", f3.Decls)
-	*/
-	/*
-		goast.Fprint(os.Stdout, token.NewFileSet(), decl, func(name string, value reflect.Value) bool {
-			return true
-		})
-	*/
-	/*
-		goast.Fprint(os.Stdout, token.NewFileSet(), decl2, func(name string, value reflect.Value) bool {
-			return true
-		})
-	*/
-
-	fmt.Printf("%+v\n", decl)
-	fmt.Printf("%+v\n", decl2)
 
 	return nil
 }
