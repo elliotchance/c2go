931470febdbee72c1f4baba7e84c01f2aa19b1b3
yulvil <p+github@pbcote.com>
2017-10-19 15:28:25 -0700

    Fixed golint warnings (comments)

--- noarch/time.go
+++ noarch/time.go
@@ -2,12 +2,15 @@ package noarch
 
 import "time"
 
+// TimeT is the representation of "time_t".
 type TimeT int32
 
+// NullToTimeT converts a NULL to an array of TimeT.
 func NullToTimeT(i int32) []TimeT {
 	return []TimeT{}
 }
 
+// Time returns the current time.
 func Time(tloc []TimeT) TimeT {
 	var t = TimeT(int32(time.Now().Unix()))
 
@@ -18,10 +21,12 @@ func Time(tloc []TimeT) TimeT {
 	return t
 }
 
+// IntToTimeT converts an int32 to a TimeT.
 func IntToTimeT(t int32) TimeT {
 	return TimeT(t)
 }
 
+// Ctime converts TimeT to a string.
 func Ctime(tloc []TimeT) []byte {
 	if len(tloc) > 0 {
 		var t = time.Unix(int64(tloc[0]), 0)
