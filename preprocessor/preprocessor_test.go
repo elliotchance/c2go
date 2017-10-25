8ce6b4a9cd6959f6a818ea3990798fc939e8ca7f
Konstantin8105 <konstantin8105@gmail.com>
2017-10-25 09:44:46 +0300

    add test for position of source

--- preprocessor/preprocessor_test.go
+++ preprocessor/preprocessor_test.go
@@ -8,28 +8,42 @@ import (
 func TestParseInclude(t *testing.T) {
 	testCases := []struct {
 		inputLine string
-		include   string
+		out       entity
 	}{
-		// TODO	add other value of entity
 		{
 			inputLine: `# 1 "/usr/include/x86_64-linux-gnu/bits/sys_errlist.h" 1 3 4`,
-			include:   "/usr/include/x86_64-linux-gnu/bits/sys_errlist.h",
+			out: entity{
+				include:          "/usr/include/x86_64-linux-gnu/bits/sys_errlist.h",
+				positionInSource: 1,
+			},
 		},
 		{
 			inputLine: `# 26 "/usr/include/x86_64-linux-gnu/bits/sys_errlist.h" 3 4`,
-			include:   "/usr/include/x86_64-linux-gnu/bits/sys_errlist.h",
+			out: entity{
+				include:          "/usr/include/x86_64-linux-gnu/bits/sys_errlist.h",
+				positionInSource: 26,
+			},
 		},
 		{
 			inputLine: `# 854 "/usr/include/stdio.h" 2 3 4`,
-			include:   "/usr/include/stdio.h",
+			out: entity{
+				include:          "/usr/include/stdio.h",
+				positionInSource: 854,
+			},
 		},
 		{
 			inputLine: `# 2 "f.c" 2`,
-			include:   "f.c",
+			out: entity{
+				include:          "f.c",
+				positionInSource: 2,
+			},
 		},
 		{
 			inputLine: `# 30 "/usr/lib/llvm-3.8/bin/../lib/clang/3.8.0/include/stdarg.h" 3 4`,
-			include:   "/usr/lib/llvm-3.8/bin/../lib/clang/3.8.0/include/stdarg.h",
+			out: entity{
+				include:          "/usr/lib/llvm-3.8/bin/../lib/clang/3.8.0/include/stdarg.h",
+				positionInSource: 30,
+			},
 		},
 	}
 	for i, tc := range testCases {
@@ -41,8 +55,11 @@ func TestParseInclude(t *testing.T) {
 			if len(actual.include) == 0 {
 				t.Fatal("Cannot parse, because result is empty")
 			}
-			if actual.include != tc.include {
-				t.Fatalf("Cannot parse line: \"%s\". Result: \"%s\". Expected: \"%s\"", tc.inputLine, actual.include, tc.include)
+			if actual.include != tc.out.include {
+				t.Fatalf("Cannot parse line: \"%s\". Result: \"%s\". Expected: \"%s\"", tc.inputLine, actual.include, tc.out.include)
+			}
+			if actual.positionInSource != tc.out.positionInSource {
+				t.Fatalf("Cannot parse source position in line: \"%s\". Result: \"%d\". Expected: \"%d\"", tc.inputLine, actual.positionInSource, tc.out.positionInSource)
 			}
 		})
 	}
