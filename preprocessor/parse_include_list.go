f4502c416e97dae16a34819fc5efc101a6d56953
Konstantin8105 <konstantin8105@gmail.com>
2017-10-26 12:31:32 +0300

    debug MacOS

--- preprocessor/parse_include_list.go
+++ preprocessor/parse_include_list.go
@@ -10,6 +10,7 @@ import (
 //    /usr/include/stdc-predef.h /usr/include/x86_64-linux-gnu/sys/cdefs.h
 func parseIncludeList(line string) (lines []string, err error) {
 	line = strings.Replace(line, "\n", " ", -1)
+	line = strings.Replace(line, "\t", " ", -1)
 	line = strings.Replace(line, "\r", " ", -1) // Added for Mac endline symbol
 	line = strings.Replace(line, "\\", " ", -1)
 	parts := strings.Split(line, " ")
