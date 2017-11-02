17e48c228c4f6b0e6507dd779bc0a732cf843499
Konstantin8105 <konstantin8105@gmail.com>
2017-11-02 13:03:53 +0300

    change recursive founding struct

--- transpiler/declarations.go
+++ transpiler/declarations.go
@@ -147,28 +147,39 @@ func transpileTypedefDecl(p *program.Program, n *ast.TypedefDecl) error {
 	   |   `-RecordType 0x24d7790 's_t'
 	   |     `-Record 0x24d7710 ''  <-- Adress of struct without name
 	*/
-	if len(n.ChildNodes) == 1 {
-		if el, ok := n.ChildNodes[0].(*ast.ElaboratedType); ok {
-			if len(el.ChildNodes) == 1 {
-				if rt, ok := el.ChildNodes[0].(*ast.RecordType); ok {
-					if len(rt.ChildNodes) == 1 {
-						if r, ok := rt.ChildNodes[0].(*ast.Record); ok {
-							if v, ok := p.StructsEmptyName[r.Addr]; ok {
-								// create like typical struct
-								var recordDecl ast.RecordDecl
-								recordDecl.Name = n.Name
-								recordDecl.ChildNodes = v
-								err := transpileRecordDecl(p, &recordDecl)
-								if err != nil {
-									return err
-								}
-								// removing from map, because now defined
-								delete(p.StructsEmptyName, n.Addr)
-								return nil
-							}
-						}
-					}
+	if len(n.ChildNodes) > 0 {
+		// find inside AST element Record
+		var deeper func([]ast.Node) (*ast.Record, error)
+
+		deeper = func(nodes []ast.Node) (*ast.Record, error) {
+			for _, n := range nodes {
+				if rec, ok := n.(*ast.Record); ok {
+					return rec, nil
 				}
+				rec, err := deeper(n.Children())
+				if err == nil {
+					return rec, nil
+				}
+			}
+			return nil, fmt.Errorf("Cannot found ast.Record")
+		}
+
+		rec, err := deeper(n.Children())
+		if err == nil {
+			if v, ok := p.StructsEmptyName[rec.Addr]; ok {
+				// create like typical struct
+				var recordDecl ast.RecordDecl
+				recordDecl.Name = n.Name
+				recordDecl.ChildNodes = v
+				err := transpileRecordDecl(p, &recordDecl)
+				if err != nil {
+					return err
+				}
+				// removing from map, because now defined
+				delete(p.StructsEmptyName, n.Addr)
+				return nil
+			} else {
+				p.AddMessage(p.GenerateWarningMessage(fmt.Errorf("Cannot found address for struct without name"), n))
 			}
 		}
 	}
