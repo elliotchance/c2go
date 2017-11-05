f2994ddfad06e8fe6951e47b3847fe77bc9da54a
Konstantin8105 <konstantin8105@gmail.com>
2017-11-05 15:54:14 +0300

    test IF is ok

--- tests/if.c
+++ tests/if.c
@@ -8,14 +8,16 @@ int main()
     int x = 1;
 
     // Without else
-    if (x == 1)
+    if (x == 1){
         pass("%s", "x is equal to one");
+	}
 
     // With else
-    if (x != 1)
+    if (x != 1){
         fail("%s", "x is not equal to one");
-    else
+	} else {
         pass("%s", "x is equal to one");
+	}
 
     done_testing();
 }
