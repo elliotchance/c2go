3f9aca2036d3ce7092e6ce0dd9419e327fd9c54b
Elliot Chance <elliotchance@gmail.com>
2017-11-18 10:13:32 +1100

    Bump version: v0.17.5 Samarium 2017-11-17

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ import (
 //     c2go -v
 //
 // See https://github.com/elliotchance/c2go/wiki/Release-Process
-const Version = "v0.17.4 Samarium 2017-11-16"
+const Version = "v0.17.5 Samarium 2017-11-17"
 
 var stderr io.Writer = os.Stderr
 
