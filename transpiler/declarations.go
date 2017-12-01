bc14bb854b539b37a8287f44e0949adf7968c553
Konstantin8105 <konstantin8105@gmail.com>
2017-12-01 20:58:50 +0300

    return

--- transpiler/declarations.go
+++ transpiler/declarations.go
@@ -260,7 +260,7 @@ func transpileTypedefDecl(p *program.Program, n *ast.TypedefDecl) (decls []goast
 func transpileVarDecl(p *program.Program, n *ast.VarDecl) (decls []goast.Decl, theType string, err error) {
 	// Ignore extern as there is no analogy for Go right now.
 	if n.IsExtern {
-		break
+		return
 	}
 
 	// There may be some startup code for this global variable.
