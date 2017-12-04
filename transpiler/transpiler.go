163bd0261929a187e9acb2a3c92e7776df9937a6
Konstantin <konstantin8105@gmail.com>
2017-12-05 01:32:09 +0300

    Refactoring: separate TranslationUnitDecl. Fixes #364 (#385)

--- transpiler/transpiler.go
+++ transpiler/transpiler.go
@@ -356,52 +356,7 @@ func transpileToNode(node ast.Node, p *program.Program) (decls []goast.Decl, err
 
 	switch n := node.(type) {
 	case *ast.TranslationUnitDecl:
-		for i := 0; i < len(n.Children()); i++ {
-			switch v := n.Children()[i].(type) {
-			case *ast.RecordDecl:
-				// specific for `typedef struct` without name
-				if v.Name != "" || i == len(n.Children())-1 {
-					var d []goast.Decl
-					d, err = transpileRecordDecl(p, v)
-					if err != nil {
-						return
-					}
-					decls = append(decls, d...)
-				}
-				for counter := 1; i+counter < len(n.Children()); counter++ {
-					if vv, ok := n.Children()[i+counter].(*ast.TypedefDecl); ok {
-						nameTypedefStruct := vv.Name
-						fields := v.Children()
-						// create a struct in according to
-						// name and fields
-						var recordDecl ast.RecordDecl
-						recordDecl.Name = nameTypedefStruct
-						recordDecl.ChildNodes = fields
-						var d []goast.Decl
-						d, err = transpileRecordDecl(p, &recordDecl)
-						if err != nil {
-							p.AddMessage(p.GenerateErrorMessage(err, node))
-							err = nil
-						} else {
-							decls = append(decls, d...)
-						}
-					} else {
-						counter--
-						i = i + counter
-						break
-					}
-				}
-			default:
-				var d []goast.Decl
-				d, err = transpileToNode(n.Children()[i], p)
-				if err != nil {
-					p.AddMessage(p.GenerateErrorMessage(err, node))
-					err = nil
-				} else {
-					decls = append(decls, d...)
-				}
-			}
-		}
+		decls, err = transpileTranslationUnitDecl(p, n)
 
 	case *ast.FunctionDecl:
 		decls, err = transpileFunctionDecl(n, p)
