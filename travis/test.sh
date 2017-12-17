8361af11939186bfc14675c617ac9f2f9e42c0db
Konstantin8105 <konstantin8105@gmail.com>
2017-12-17 14:47:21 +0300

    DEBUG

--- travis/test.sh
+++ travis/test.sh
@@ -8,9 +8,9 @@ function cleanup {
     EXIT_STATUS=$?
 
     if [ $EXIT_STATUS != 0 ]; then
+		sed -n '1600,1690p;1691q' ./build/tests/struct/main_test.go >> $OUTFILE
         [ ! -f $OUTFILE ] || cat $OUTFILE
 		# DEBUG
-		sed -n '1600,1690p;1691q' ./build/tests/struct/main_test.go
     fi
 
     exit $EXIT_STATUS
