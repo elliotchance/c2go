776028fa56b8c3e7596abcc9f800467128e86cdc
Elliot Chance <elliotchance@gmail.com>
2017-12-27 10:48:59 +1100

    Added strcpy() and strncpy(). Fixes #259 (#435)

---
+++ darwin/string.go
@@ -0,0 +1,21 @@
+package darwin
+
+import "github.com/elliotchance/c2go/noarch"
+
+// BuiltinStrcpy is for __builtin___strcpy_chk.
+// https://opensource.apple.com/source/Libc/Libc-498/include/secure/_string.h
+func BuiltinStrcpy(dest, src []byte, size int) []byte {
+	return noarch.Strcpy(dest, src)
+}
+
+// BuiltinObjectSize is for __builtin_object_size.
+// https://github.com/elliotchance/c2go/issues/359
+func BuiltinObjectSize(ptr []byte, theType int) int {
+	return 5
+}
+
+// BuiltinStrncpy is for __builtin___strncpy_chk.
+// https://opensource.apple.com/source/Libc/Libc-498/include/secure/_string.h
+func BuiltinStrncpy(dest, src []byte, len, size int) []byte {
+	return noarch.Strncpy(dest, src, len)
+}
--- noarch/string.go
+++ noarch/string.go
@@ -12,3 +12,50 @@ func Strlen(a []byte) int {
 
 	return len(CStringToString(a))
 }
+
+// Strcpy copies the C string pointed by source into the array pointed by
+// destination, including the terminating null character (and stopping at that
+// point).
+//
+// To avoid overflows, the size of the array pointed by destination shall be
+// long enough to contain the same C string as source (including the terminating
+// null character), and should not overlap in memory with source.
+func Strcpy(dest, src []byte) []byte {
+	for i, c := range src {
+		dest[i] = c
+
+		// We only need to copy until the first NULL byte. Make sure we also
+		// include that NULL byte on the end.
+		if c == 0 {
+			break
+		}
+	}
+
+	return dest
+}
+
+// Strncpy copies the first num characters of source to destination. If the end
+// of the source C string (which is signaled by a null-character) is found
+// before num characters have been copied, destination is padded with zeros
+// until a total of num characters have been written to it.
+//
+// No null-character is implicitly appended at the end of destination if source
+// is longer than num. Thus, in this case, destination shall not be considered a
+// null terminated C string (reading it as such would overflow).
+//
+// destination and source shall not overlap (see memmove for a safer alternative
+// when overlapping).
+func Strncpy(dest, src []byte, len int) []byte {
+	// Copy up to the len or first NULL bytes - whichever comes first.
+	i := 0
+	for ; i < len && src[i] != 0; i++ {
+		dest[i] = src[i]
+	}
+
+	// The rest of the dest will be padded with zeros to the len.
+	for ; i < len; i++ {
+		dest[i] = 0
+	}
+
+	return dest
+}
