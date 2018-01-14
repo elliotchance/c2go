901efa7baf15ae0a66e3bc2cfd30f033f1f8ddf3
Elliot Chance <elliotchance@gmail.com>
2018-01-14 13:29:34 +1100

    Bump version: v0.21.1 Zinc 2018-01-14

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ import (
 //     c2go -v
 //
 // See https://github.com/elliotchance/c2go/wiki/Release-Process
-const Version = "v0.21.0 Zinc 2018-01-11"
+const Version = "v0.21.1 Zinc 2018-01-14"
 
 var stderr io.Writer = os.Stderr
 
