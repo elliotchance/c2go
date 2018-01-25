6147f55c82555de46fdf2c97fdec0d770ef01768
Konstantin8105 <konstantin8105@gmail.com>
2018-01-25 15:39:58 +0300

    simplification union test

--- tests/union.c
+++ tests/union.c
@@ -3,54 +3,26 @@
 #include <stdio.h>
 #include "tests.h"
 
-union programming
+
+union simple_union
 {
-    float constant;
-    char *pointer;
+	int a;
+	int b;
 };
 
-union programming init_var()
+void union_simple()
 {
-    union programming variable;
-    char *s = "Programming in Software Development.";
-
-    variable.pointer = s;
-    is_streq(variable.pointer, "Programming in Software Development.");
-
-    variable.constant = 1.23;
-    is_eq(variable.constant, 1.23);
-
-    return variable;
-}
-
-void pass_by_ref(union programming *addr)
-{
-    char *s = "Show string member.";
-    float v = 1.23+4.56;
-
-    addr->constant += 4.56;
-    is_eq(addr->constant, v);
-
-    addr->pointer = s;
-    is_streq(addr->pointer, "Show string member.");
-}
-
-void var_by_val(union programming value)
-{
-    value.constant++;
-
-    is_eq(value.constant, 2.23);
+	union simple_union s;
+	s.a = 43;
+	is_eq( s.a , 43 );
+	is_eq( s.b , 43 );
 }
 
 int main()
 {
-    plan(5);
+    plan(2);
 
-    union programming variable;
-
-    variable = init_var();
-    var_by_val(variable);
-    pass_by_ref(&variable);
+	union_simple();
 
     done_testing();
 }
--- tests/union_in_struct.c
+++ tests/union_in_struct.c
@@ -69,9 +69,55 @@ void union_inside_struct2()
 	is_eq((*f.u.pHash).i , 100);
 }
 
+
+union programming
+{
+    float constant;
+    char *pointer;
+};
+
+union programming init_var()
+{
+    union programming variable;
+    char *s = "Programming in Software Development.";
+
+    variable.pointer = s;
+    is_streq(variable.pointer, "Programming in Software Development.");
+
+    variable.constant = 1.23;
+    is_eq(variable.constant, 1.23);
+
+    return variable;
+}
+
+void pass_by_ref(union programming *addr)
+{
+    char *s = "Show string member.";
+    float v = 1.23+4.56;
+
+    addr->constant += 4.56;
+    is_eq(addr->constant, v);
+
+    addr->pointer = s;
+    is_streq(addr->pointer, "Show string member.");
+}
+
+void var_by_val(union programming value)
+{
+    value.constant++;
+
+    is_eq(value.constant, 2.23);
+}
+
 int main()
 {
-    plan(11);
+    plan(16);
+    
+	union programming variable;
+
+    variable = init_var();
+    var_by_val(variable);
+    pass_by_ref(&variable);
 	
 	union_inside_struct();
 	union_inside_struct2();
