85d3ce8154936c33588db7e374065d95c309128f
Konstantin8105 <konstantin8105@gmail.com>
2018-01-25 15:51:17 +0300

    add test union with array

--- tests/union.c
+++ tests/union.c
@@ -3,26 +3,54 @@
 #include <stdio.h>
 #include "tests.h"
 
-
-union simple_union
+union int_union
 {
 	int a;
 	int b;
 };
 
+union float_union
+{
+	float a;
+	float b;
+};
+
 void union_simple()
 {
-	union simple_union s;
-	s.a = 43;
-	is_eq( s.a , 43 );
-	is_eq( s.b , 43 );
+	union int_union i;
+	i.a = 43;
+	is_eq( i.a , 43 );
+	is_eq( i.b , 43 );
+
+	union float_union f;
+	f.a = 45;
+	is_eq( f.a , 45 );
+	is_eq( f.b , 45 );
+}
+
+union array_union
+{
+	float a[2];
+	float b[2];
+};
+
+void union_array()
+{
+	union array_union arr;
+	arr.a[0] = 12;
+	arr.b[1] = 14;
+	is_eq( arr.a[0] , 12);
+	is_eq( arr.a[1] , 14);
+	is_eq( arr.b[0] , 12);
+	is_eq( arr.b[1] , 14);
 }
 
 int main()
 {
-    plan(2);
+    plan(8);
 
 	union_simple();
+	union_array ();
 
     done_testing();
 }
