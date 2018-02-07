2923429bc5ece51ce65a9c5056a58263766473cd
Elliot Chance <elliotchance@gmail.com>
2018-02-07 13:03:49 +1100

    Add a test case for VerbatimLineComment

--- ast/verbatim_line_comment.go
+++ ast/verbatim_line_comment.go
@@ -5,12 +5,17 @@ type VerbatimLineComment struct {
 	Addr       Address
 	Pos        Position
 	Text       string
+	Name       string
+	CloseName  string
 	ChildNodes []Node
 }
 
 func parseVerbatimLineComment(line string) *VerbatimLineComment {
 	groups := groupsFromRegex(
-		`<(?P<position>.*)> Text="(?P<text>.*)"`,
+		`<(?P<position>.*)>
+		( Text="(?P<text>.*?)")?
+		( Name="(?P<name>.*?)")?
+		( CloseName="(?P<close_name>.*?)")?`,
 		line,
 	)
 
@@ -18,6 +23,8 @@ func parseVerbatimLineComment(line string) *VerbatimLineComment {
 		Addr:       ParseAddress(groups["address"]),
 		Pos:        NewPositionFromString(groups["position"]),
 		Text:       groups["text"],
+		Name:       groups["name"],
+		CloseName:  groups["close_name"],
 		ChildNodes: []Node{},
 	}
 }
