a406d16b4016de480cc29b2ad18bc5e4c1665676
Elliot Chance <elliotchance@gmail.com>
2018-02-15 09:18:49 +1100

    Bump version: v0.21.10 Zinc 2018-02-14

--- program/version.go
+++ program/version.go
@@ -5,4 +5,4 @@ package program
 //     c2go -v
 //
 // See https://github.com/elliotchance/c2go/wiki/Release-Process
-const Version = "v0.21.9 Zinc 2018-02-13"
+const Version = "v0.21.10 Zinc 2018-02-14"
