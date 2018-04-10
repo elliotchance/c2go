8ce69234012e9b5a75ca6913d645913071503e41
Elliot Chance <elliotchance@gmail.com>
2018-04-11 09:37:25 +1000

    Bump version: v0.22.1 Aluminium 2018-04-10

--- program/version.go
+++ program/version.go
@@ -5,4 +5,4 @@ package program
 //     c2go -v
 //
 // See https://github.com/elliotchance/c2go/wiki/Release-Process
-const Version = "v0.22.0 Aluminium 2018-04-07"
+const Version = "v0.22.1 Aluminium 2018-04-10"
