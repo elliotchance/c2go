b0b57210610a1df0087914f0ca8b24444fbed6a6
Elliot Chance <elliotchance@gmail.com>
2018-05-04 10:24:59 +1000

    Bump version: v0.23.3 Berkelium 2018-05-04

--- program/version.go
+++ program/version.go
@@ -5,4 +5,4 @@ package program
 //     c2go -v
 //
 // See https://github.com/elliotchance/c2go/wiki/Release-Process
-const Version = "v0.23.2 Berkelium 2018-05-03"
+const Version = "v0.23.3 Berkelium 2018-05-04"
