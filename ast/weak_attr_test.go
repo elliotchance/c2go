4f99007ee738616c6288a5701c399424799d83ab
Elliot Chance <elliotchance@gmail.com>
2018-05-06 12:18:19 +1000

    Fix panic on WeakAttr. Fixes #610

--- ast/weak_attr_test.go
+++ ast/weak_attr_test.go
@@ -9,6 +9,13 @@ func TestWeakAttr(t *testing.T) {
 		`0x56069ece5110 <line:736:22>`: &WeakAttr{
 			Addr:       0x56069ece5110,
 			Pos:        NewPositionFromString("line:736:22"),
+			Inherited:  false,
+			ChildNodes: []Node{},
+		},
+		`0x20c6ad0 </glibc-2.27/support/temp_file-internal.h:27:62> Inherited`: &WeakAttr{
+			Addr:       0x20c6ad0,
+			Pos:        NewPositionFromString("/glibc-2.27/support/temp_file-internal.h:27:62"),
+			Inherited:  true,
 			ChildNodes: []Node{},
 		},
 	}
