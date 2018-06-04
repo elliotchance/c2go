95503a22a084e5c18998e8b246d9b4f8895dd5e5
Elliot Chance <elliotchance@gmail.com>
2018-06-04 10:06:58 +1000

    Bump version: v0.24.4 Cadmium 2018-06-04

--- program/version.go
+++ program/version.go
@@ -5,4 +5,4 @@ package program
 //     c2go -v
 //
 // See https://github.com/elliotchance/c2go/wiki/Release-Process
-const Version = "v0.24.3 Cadmium 2018-05-28"
+const Version = "v0.24.4 Cadmium 2018-06-04"
