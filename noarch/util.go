e7b438ec54f254fbc94c53e888a18c1163a73812
Christophe Kamphaus <christophe.kamphaus@jemmic.com>
2018-06-14 15:16:42 +0200

    Refactored library functions to use pointers

--- noarch/util.go
+++ noarch/util.go
@@ -8,48 +8,45 @@ import (
 
 // CStringToString returns a string that contains all the bytes in the
 // provided C string up until the first NULL character.
-func CStringToString(s []byte) string {
+func CStringToString(s *byte) string {
 	if s == nil {
 		return ""
 	}
 
 	end := -1
-	for i, b := range s {
-		if b == 0 {
+	for i := 0; ; i++ {
+		if *(*byte)(unsafe.Pointer(uintptr(unsafe.Pointer(s)) + uintptr(i))) == 0 {
 			end = i
 			break
 		}
 	}
 
 	if end == -1 {
-		end = len(s)
+		return ""
 	}
 
-	newSlice := make([]byte, end)
-	copy(newSlice, s)
-
-	return string(newSlice)
+	return string(toByteSlice(s, end))
 }
 
 // StringToCString returns the C string (also known as a null terminated string)
 // to be as used as a string in C.
-func StringToCString(s string) []byte {
+func StringToCString(s string) *byte {
 	cString := make([]byte, len(s)+1)
 	copy(cString, []byte(s))
 	cString[len(s)] = 0
 
-	return cString
+	return &cString[0]
 }
 
 // CStringIsNull will test if a C string is NULL. This is equivalent to:
 //
 //    s == NULL
-func CStringIsNull(s []byte) bool {
-	if s == nil || len(s) < 1 {
+func CStringIsNull(s *byte) bool {
+	if s == nil {
 		return true
 	}
 
-	return s[0] == 0
+	return *s == 0
 }
 
 // CPointerToGoPointer converts a C-style pointer into a Go-style pointer.
@@ -63,6 +60,16 @@ func CPointerToGoPointer(a interface{}) interface{} {
 	t := reflect.TypeOf(a).Elem()
 
 	return reflect.New(t).Elem().Addr().Interface()
+}
+
+// toByteSlice returns a byte slice to a with the given length.
+func toByteSlice(a *byte, length int) []byte {
+	header := reflect.SliceHeader{
+		uintptr(unsafe.Pointer(a)),
+		length,
+		length,
+	}
+	return (*(*[]byte)(unsafe.Pointer(&header)))[:]
 }
 
 // GoPointerToCPointer does the opposite of CPointerToGoPointer.
