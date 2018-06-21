af75cd912f73adc26ebbf4a29a8a0eba909d01d9
kamphaus <christophe.kamphaus@gmail.com>
2018-06-22 01:49:57 +0200

    Use nop logger on platforms without syslog support. Fixes #767 (#768)

---
+++ noarch/syslog_nop.go
@@ -0,0 +1,25 @@
+// +build windows nacl plan9
+// A nop syslog logger for platforms with no syslog support
+
+package noarch
+
+// void    closelog(void);
+func Closelog() {
+}
+
+// void    openlog(const char *, int, int);
+func Openlog(ident []byte, logopt int32, facility int32) {
+}
+
+// int     setlogmask(int);
+func Setlogmask(mask int32) int32 {
+	return 0
+}
+
+// void    syslog(int, const char *, ...);
+func Syslog(priority int32, format []byte, args ...interface{}) {
+}
+
+// void    vsyslog(int, const char *, struct __va_list_tag *);
+func Vsyslog(priority int32, format []byte, args VaList) {
+}
