d11c3760558d037512e290aa02cac838a05f8266
Elliot Chance <elliotchance@gmail.com>
2018-06-26 09:32:04 +1000

    Bump version: v0.24.8 Caesium 2018-06-25

--- program/version.go
+++ program/version.go
@@ -5,4 +5,4 @@ package program
 //     c2go -v
 //
 // See https://github.com/elliotchance/c2go/wiki/Release-Process
-const Version = "v0.24.7 Caesium 2018-06-21"
+const Version = "v0.24.8 Caesium 2018-06-25"
