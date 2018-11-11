3c8f0b9f63d27f7e89a638975baf66d907e90699
Elliot Chance <elliotchance@gmail.com>
2018-11-11 19:03:55 +1100

    Bump version: v0.25.4 Dubnium 2018-11-11

--- program/version.go
+++ program/version.go
@@ -5,4 +5,4 @@ package program
 //     c2go -v
 //
 // See https://github.com/elliotchance/c2go/wiki/Release-Process
-const Version = "v0.25.3 Dubnium 2018-11-11"
+const Version = "v0.25.4 Dubnium 2018-11-11"
