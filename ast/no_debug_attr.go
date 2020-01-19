09d2c968ce8f23169ea999df5a93ff93b65a2202
cornelk <github@codejuggle.dj>
2020-01-19 19:41:53 +0700

    Add support for NoDebugAttr

---
+++ ast/no_debug_attr.go
@@ -0,0 +1,45 @@
+package ast
+
+// NoDebugAttr is a type of attribute that is optionally attached to a variable
+// or struct field definition.
+type NoDebugAttr struct {
+	Addr       Address
+	Pos        Position
+	ChildNodes []Node
+}
+
+func parseNoDebugAttr(line string) *NoDebugAttr {
+	groups := groupsFromRegex(
+		`<(?P<position>.*)>`,
+		line,
+	)
+
+	return &NoDebugAttr{
+		Addr:       ParseAddress(groups["address"]),
+		Pos:        NewPositionFromString(groups["position"]),
+		ChildNodes: []Node{},
+	}
+}
+
+// AddChild adds a new child node. Child nodes can then be accessed with the
+// Children attribute.
+func (n *NoDebugAttr) AddChild(node Node) {
+	n.ChildNodes = append(n.ChildNodes, node)
+}
+
+// Address returns the numeric address of the node. See the documentation for
+// the Address type for more information.
+func (n *NoDebugAttr) Address() Address {
+	return n.Addr
+}
+
+// Children returns the child nodes. If this node does not have any children or
+// this node does not support children it will always return an empty slice.
+func (n *NoDebugAttr) Children() []Node {
+	return n.ChildNodes
+}
+
+// Position returns the position in the original source code.
+func (n *NoDebugAttr) Position() Position {
+	return n.Pos
+}
