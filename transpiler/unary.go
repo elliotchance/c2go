6fb06d7a8d1eed1b98c7d0d78fed74801dbc8cdc
Tyler Kropp <tyler@tylerkropp.xyz>
2020-07-26 14:24:01 -0400

    Support UnaryExprOrTypeTraitExpr->ConditionalOperator
    Fixes #868

--- transpiler/unary.go
+++ transpiler/unary.go
@@ -720,6 +720,9 @@ func transpileUnaryExprOrTypeTraitExpr(n *ast.UnaryExprOrTypeTraitExpr, p *progr
 			case *ast.CStyleCastExpr:
 				t = ty.Type
 
+			case *ast.ConditionalOperator:
+				t = ty.Type
+
 			default:
 				panic(fmt.Sprintf("cannot do unary on: %#v", ty))
 			}
