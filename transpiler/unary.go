aeeb3e1018bfce6a61e58b1d9d41eb0524e68194
Tyler Kropp <tyler@tylerkropp.xyz>
2020-07-26 16:28:18 -0400

    Support UnaryExprOrTypeTraitExpr->StringLiteral
    Fixes #823

--- transpiler/unary.go
+++ transpiler/unary.go
@@ -729,6 +729,9 @@ func transpileUnaryExprOrTypeTraitExpr(n *ast.UnaryExprOrTypeTraitExpr, p *progr
 			case *ast.IntegerLiteral:
 				t = ty.Type
 
+			case *ast.StringLiteral:
+				t = ty.Type
+
 			default:
 				panic(fmt.Sprintf("cannot do unary on: %#v", ty))
 			}
